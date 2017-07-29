f0e3a3877ff1eb1ca803c013ac54f71869550f79
Edward A Maxedon <edadma@gmail.com>
2017-07-29 19:43:11 -0400

    add importable table

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,7 +10,40 @@ object Main extends App {
 
 	val inserts = new ArrayBuffer[Insert]
 	val mdout = new PrintWriter( "northwind.md" )
+
+	def mdprint( heading: String, columns: Vector[String], right: List[Int], name: String ) = {
+		mdout.println( s"## $heading" )
+		mdout.println
+		mdout.print(
+			new TextTable( markdown = true ) {
+				headerSeq( columns )
+
+				for (c <- right)
+					rightAlignment( c )
+
+				for (Insert( table, row ) <- inserts if table.name == name)
+					rowSeq( row )
+			} )
+		mdout.println
+		mdout.println
+	}
+
 	val tabout = new PrintWriter( "northwind.tab" )
+
+	def tabprint( heading: String, columns: Vector[String], right: List[Int], name: String ) = {
+		tabout.println( heading )
+		tabout.print(
+			new TextTable( headerUnderlined = false, headerBold = false ) {
+				headerSeq( columns )
+
+				for (c <- right)
+					rightAlignment( c )
+
+				for (Insert( table, row ) <- inserts if table.name == name)
+					rowSeq( row )
+			} )
+		tabout.println
+	}
 
 	for (ins <- InsertParser.parseStatement( io.Source.fromFile("northwind.in") mkString ))
 		inserts += ins
@@ -19,18 +52,8 @@ object Main extends App {
 	for (ins@Insert( table, row ) <- inserts if table.name == "categories")
 		ins.row = row.updated(3, s"pic${row(0)}.jpg")
 
-	mdout.println( "## Categories" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "CategoryID", "CategoryName", "Description", "Picture" ) )
-			rightAlignment( 1 )
-
-			for (Insert( table, row ) <- inserts if table.name == "categories")
-				rowSeq( row )
-		} )
-	mdout.println
-	mdout.println
+	mdprint( "Categories", Vector("CategoryID", "CategoryName", "Description", "Picture"), List(1), "categories" )
+	tabprint( "Categories", Vector("CategoryID:integer", "CategoryName", "Description", "Picture"), List(1), "categories" )
 
 	//////////////////////// customers
 	val customerids = new HashMap[String, Int]
@@ -52,37 +75,17 @@ object Main extends App {
 				ins.row = row.updated( 1, id.toString )
 		}
 
-	mdout.println( "## Customers" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "CustomerID", "CompanyName", "ContactName", "ContactTitle", "Address", "City", "Region", "PostalCode", "Country", "Phone", "Fax" ) )
-			rightAlignment( 1 )
-
-			for (Insert( table, row ) <- inserts if table.name == "customers")
-				rowSeq( row )
-		} )
-	mdout.println
-	mdout.println
+	mdprint( "Customers", Vector("CustomerID", "CompanyName", "ContactName", "ContactTitle", "Address", "City", "Region", "PostalCode", "Country", "Phone", "Fax"), List(1), "customers" )
+	tabprint( "Customers", Vector("CustomerID:integer", "CompanyName", "ContactName", "ContactTitle", "Address", "City", "Region", "PostalCode", "Country", "Phone", "Fax"), List(1), "customers" )
 
 	//////////////////////// employees
 	for (ins@Insert( table, row ) <- inserts if table.name == "employees")
 		ins.row = row.slice(0, 14) ++ row.slice(15, row.length)
 
-	mdout.println( "## Employees" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "EmployeeID", "LastName", "FirstName", "Title", "TitleOfCourtesy", "BirthDate", "HireDate",
-				"Address", "City", "Region", "PostalCode", "Country", "HomePhone", "Extension", "Notes", "ReportsTo", "Photopath" ) )
-			rightAlignment( 1 )
-			rightAlignment( 16 )
-
-			for (Insert( table, row ) <- inserts if table.name == "employees")
-				rowSeq( row )
-		} )
-	mdout.println
-	mdout.println
+	mdprint( "Employees", Vector("EmployeeID", "LastName", "FirstName", "Title", "TitleOfCourtesy", "BirthDate", "HireDate",
+		"Address", "City", "Region", "PostalCode", "Country", "HomePhone", "Extension", "Notes", "ReportsTo", "Photopath"), List(1, 16), "employees" )
+	tabprint( "Employees", Vector("EmployeeID:integer", "LastName", "FirstName", "Title", "TitleOfCourtesy", "BirthDate:dateUS", "HireDate:dateUS",
+		"Address", "City", "Region", "PostalCode", "Country", "HomePhone", "Extension", "Notes", "ReportsTo", "Photopath"), List(1, 16), "employees" )
 
 	//////////////////////// territories
 	val territoryids = new HashMap[String, Int]
@@ -107,35 +110,12 @@ object Main extends App {
 				nextemployeeterritoryid += 1
 		}
 
-	mdout.println( "## Territories" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "TerritoryID", "Territory", "TerritoryDescription", "RegionID" ) )
-			rightAlignment( 1 )
-			rightAlignment( 4 )
-
-			for (Insert( table, row ) <- inserts if table.name == "territories")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "Territories", Vector("TerritoryID", "Territory", "TerritoryDescription", "RegionID"), List(1, 4), "territories" )
+	tabprint( "Territories", Vector("TerritoryID:integer", "Territory", "TerritoryDescription", "RegionID:integer"), List(1, 4), "territories" )
 
 	//////////////////////// employeeterritories
-	mdout.println( "## EmployeeTerritories" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "EmployeeTerritoryID", "EmployeeID", "TerritoryID" ) )
-			rightAlignment( 1 )
-			rightAlignment( 2 )
-			rightAlignment( 3 )
-
-			for (Insert( table, row ) <- inserts if table.name == "employeeterritories")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "EmployeeTerritories", Vector("EmployeeTerritoryID", "EmployeeID", "TerritoryID"), List(1, 2, 3), "employeeterritories" )
+	tabprint( "EmployeeTerritories", Vector("EmployeeTerritoryID:integer", "EmployeeID:integer", "TerritoryID:integer"), List(1, 2, 3), "employeeterritories" )
 
 	//////////////////////// order_details
 	var order_detailid = 1
@@ -145,106 +125,29 @@ object Main extends App {
 		order_detailid += 1
 	}
 
-	mdout.println( "## OrderDetails" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "OrderDetailID", "OrderID", "ProductID", "UnitPrice", "Quantity", "Discount" ) )
-			rightAlignment( 1 )
-			rightAlignment( 2 )
-			rightAlignment( 3 )
-			rightAlignment( 4 )
-			rightAlignment( 5 )
-			rightAlignment( 6 )
-
-			for (Insert( table, row ) <- inserts if table.name == "order_details")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "OrderDetails", Vector("OrderDetailID", "OrderID", "ProductID", "UnitPrice", "Quantity", "Discount"), List(1, 2, 3, 4, 5, 6), "order_details" )
+	tabprint( "OrderDetails", Vector("OrderDetailID:integer", "OrderID:integer", "ProductID:integer", "UnitPrice:decimal", "Quantity:integer", "Discount:decimal"), List(1, 2, 3, 4, 5, 6), "order_details" )
 
 	//////////////////////// orders
-	mdout.println( "## Orders" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "OrderID", "CustomerID", "EmployeeID", "OrderDate", "RequiredDate", "ShippedDate", "ShipVia", "Freight", "ShipName", "ShipAddress", "ShipCity", "ShipRegion", "ShipPostalCode", "ShipCountry" ) )
-			rightAlignment( 1 )
-			rightAlignment( 2 )
-			rightAlignment( 3 )
-			rightAlignment( 7 )
-			rightAlignment( 8 )
-
-			for (Insert( table, row ) <- inserts if table.name == "orders")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "Orders", Vector("OrderID", "CustomerID", "EmployeeID", "OrderDate", "RequiredDate", "ShippedDate", "ShipVia", "Freight", "ShipName", "ShipAddress", "ShipCity", "ShipRegion", "ShipPostalCode", "ShipCountry"), List(1, 2, 3, 7, 8), "orders" )
+	tabprint( "Orders", Vector("OrderID:integer", "CustomerID:integer", "EmployeeID:integer", "OrderDate:date", "RequiredDate:date", "ShippedDate:date", "ShipVia", "Freight:decimal", "ShipName", "ShipAddress", "ShipCity", "ShipRegion", "ShipPostalCode", "ShipCountry"), List(1, 2, 3, 7, 8), "orders" )
 
 	//////////////////////// products
-	mdout.println( "## Products" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "ProductID", "ProductName", "SupplierID", "CategoryID", "QuantityPerUnit", "UnitPrice", "UnitsInStock", "UnitsOnOrder", "ReorderLevel", "Discontinued" ) )
-			rightAlignment( 1 )
-			rightAlignment( 3 )
-			rightAlignment( 4 )
-			rightAlignment( 5 )
-			rightAlignment( 6 )
-			rightAlignment( 7 )
-			rightAlignment( 8 )
-			rightAlignment( 9 )
-			rightAlignment( 10 )
-
-			for (Insert( table, row ) <- inserts if table.name == "products")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "Products", Vector("ProductID", "ProductName", "SupplierID", "CategoryID", "QuantityPerUnit", "UnitPrice", "UnitsInStock", "UnitsOnOrder", "ReorderLevel", "Discontinued"), List(1, 3, 4, 5, 6, 7, 8, 9), "products" )
+	tabprint( "Products", Vector("ProductID:integer", "ProductName", "SupplierID:integer", "CategoryID:integer", "QuantityPerUnit:decimal", "UnitPrice:decimal", "UnitsInStock:integer", "UnitsOnOrder:integer", "ReorderLevel:integer", "Discontinued:integer"), List(1, 3, 4, 5, 6, 7, 8, 9), "products" )
 
 	//////////////////////// region
-	mdout.println( "## Regions" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "RegionID", "RegionDescription" ) )
-			rightAlignment( 1 )
-
-			for (Insert( table, row ) <- inserts if table.name == "region")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "Regions", Vector("RegionID", "RegionDescription"), List(1), "region" )
+	tabprint( "Regions", Vector("RegionID:integer", "RegionDescription"), List(1), "region" )
 
 	//////////////////////// shippers
-	mdout.println( "## Shippers" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "ShipperID", "CompanyName", "Phone" ) )
-			rightAlignment( 1 )
-
-			for (Insert( table, row ) <- inserts if table.name == "shippers")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
+	mdprint( "Shippers", Vector("ShipperID", "CompanyName", "Phone"), List(1), "shippers" )
+	tabprint( "Shippers", Vector("ShipperID:integer", "CompanyName", "Phone"), List(1), "shippers" )
 
 	//////////////////////// suppliers
-	mdout.println( "## Suppliers" )
-	mdout.println
-	mdout.print(
-		new TextTable( markdown = true ) {
-			headerSeq( Vector( "SupplierID", "CompanyName", "ContactName", "ContactTitle", "Address", "City", "Region", "PostalCode", "Country", "Phone", "Fax", "Homepage" ) )
-			rightAlignment( 1 )
-
-			for (Insert( table, row ) <- inserts if table.name == "suppliers")
-				rowSeq( row )
-		}	)
-	mdout.println
-	mdout.println
-
+	mdprint( "Suppliers", Vector("SupplierID", "CompanyName", "ContactName", "ContactTitle", "Address", "City", "Region", "PostalCode", "Country", "Phone", "Fax", "Homepage"), List(1), "suppliers" )
+	tabprint( "Suppliers", Vector("SupplierID:integer", "CompanyName", "ContactName", "ContactTitle", "Address", "City", "Region", "PostalCode", "Country", "Phone", "Fax", "Homepage"), List(1), "suppliers" )
 
 	mdout.close
+	tabout.close
 }
